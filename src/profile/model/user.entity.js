4d6f4c6d1d4a294267554d2c59eec61aa7f49384
OmBRz <omarbrrcl@gmail.com>
2024-04-26 18:25:27 -0500

    feat(users): added users entity.

---
+++ src/profile/model/user.entity.js
@@ -0,0 +1,10 @@
+export class User{
+    constructor(id, name, lastname, email, phone, urlToImage){
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.phone = phone;
+        this.urlToImage = urlToImage;
+    }
+}
