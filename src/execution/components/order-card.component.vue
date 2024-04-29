42374f85c7863efdbe37b7099ad23dd3e233320f
RubenGenaro10 <rubengenaro10@gmail.com>
2024-04-28 19:00:55 -0500

    feat: Added order card

---
+++ src/execution/components/order-card.component.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "order-card.component"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/execution/pages/make-order.component.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "make-order.component"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
