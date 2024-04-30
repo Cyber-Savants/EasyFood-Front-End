0c7920de22d7772b2dcf121a2b844ad604272b41
RubenGenaro10 <rubengenaro10@gmail.com>
2024-04-30 07:32:26 -0500

    feat: Added Home in progress

--- src/public/components/home-content.component.vue
+++ src/public/components/home-content.component.vue
@@ -10,8 +10,8 @@ export default {
      <img src="../../assets/en-proceso.jpg" alt="Trabajando">
    </div>
    <div class="mx-8 justify-content-center pt-8">
-     <h1 class="flex justify-content-center">Este contenido se vera el futuros sprints</h1>
-     <h2 class="flex justify-content-center">Estamos trabajando en ello</h2>
+     <h1 class="flex justify-content-center">Este contenido se vera en futuros sprints</h1>
+     <h2 class="flex justify-content-center">Estamos trabajando en ello :(</h2>
      <h3 class="flex justify-content-center">Gracias por su paciencia</h3>
    </div>
  </div>
