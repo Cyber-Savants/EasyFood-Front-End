92a2f6572a2def41a954498fae0e6a0308f8fa7a
OmBRz <omarbrrcl@gmail.com>
2024-04-30 06:11:41 -0500

    feat(profile-component): Added user card component

---
+++ src/profile/components/user-data-card.component.vue
@@ -0,0 +1,40 @@
+<script>
+import {User} from "../model/user.entity.js";
+
+export default {
+  name: "user-data-card",
+  props: {
+    user: {
+      type: User,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-card class="mt-5 md:m-5" style="width: 50%; background-color: #97E896 ;overflow: hidden">
+    <template #title>Mis Datos</template>
+    <template #content>
+      <p class="m-0">
+        <span class="font-bold">Nombre:</span>
+        <p>{{user.name}}</p>
+        <span class="font-bold">Apellido:</span>
+        <p>{{user.lastname}}</p>
+        <span class="font-bold">Email:</span>
+        <p>{{user.email}}</p>
+        <span class="font-bold">Telefono:</span>
+        <p>{{user.phone}}</p>
+      </p>
+    </template>
+    <template #footer>
+      <div class="flex gap-3 mt-1">
+        <pv-button label="Editar" style="background-color: white; color: black" />
+      </div>
+    </template>
+  </pv-card>
+</template>
+
+<style scoped>
+
+</style>
--- src/profile/pages/user-profile.component.vue
+++ src/profile/pages/user-profile.component.vue
@@ -1,35 +1,81 @@
 <script>
 import {User} from "../model/user.entity.js";
+import UserDataCard from "../components/user-data-card.component.vue";
+import {UsersApiService} from "../services/users-api.service.js";
 
 export default {
   name: "user-profile",
-  props: {
-    user: {
-      type: User,
-      required: true
+  components: {UserDataCard},
+  data(){
+    return{
+      user: User,
+      errors: [],
+      usersApi: new UsersApiService()
     }
-  }
+  },
+  created() {
+    this.getAllUsers();
+  },
+  methods:{
+    buildUserFromResponsiveData(user){
+      return new User(
+          user.name,
+          user.lastname,
+          user.email,
+          user.phone,
+          user.address,
+          user.urlToImage
+      )
+    },
 
+    getAllUsers(){
+      this.usersApi.getAllUsers()
+          .then(response =>{
+            let user = response.data;
+            this.user = this.buildUserFromResponsiveData(user);
+            console.log(response.data);
+          })
+          .catch(error => {
+            this.errors.push(error);
+          });
+    }
+
+  }
 }
 </script>
 
 <template>
-  <div class="bg-green-200">
-    <div class="flex align-items-center justify-content-center">
-      <img v-if="user" :src="user.urlToImage" :alt="user.name" style="font-size: 5rem">
+  <div class="profile-container">
+    <div class="flex mt-5 flex-column align-items-center">
+      <div>
+        <img v-if="user" :src="user.urlToImage" :alt="user.name">
+        <p>{{ user.name }} {{user.lastname}}</p>
+      </div>
+      <div class="button-container">
+        <pv-button class="custom-button" label="Mis Datos"/>
+        <pv-button class="custom-button" label="Direcciones"/>
+        <pv-button class="custom-button" label="Historial de Pedidos"/>
+        <pv-button class="custom-button" label="Ayuda"/>
+      </div>
     </div>
-    <div class="button-container">
-      <pv-button class="custom-button" label="Mis Datos"></pv-button>
-      <pv-button class="custom-button" label="Direcciones"></pv-button>
-      <pv-button class="custom-button" label="Historial de Pedidos"></pv-button>
-      <pv-button class="custom-button" label="Ayuda"></pv-button>
-    </div>
+    <UserDataCard :user="user"></UserDataCard>
   </div>
 </template>
 
 <style scoped>
+.profile-container {
+  display: flex;
+  background-color: #d2f0cd;
+  gap: 3rem;
+}
+
+img {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+}
 .custom-button {
-  background-color: #A8D291;
+  background-color: #97E896;
   border: none;
   color: black;
   text-align: center;
@@ -47,7 +93,7 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 5rem;
-  gap: 2rem;
+  padding: 3rem;
+  gap: 1rem;
 }
 </style>
